f5a28fe3e9c65265d518e82719853db87a75ad99
jldaggs88 <daggs.jessa@gmail.com>
2020-10-26 02:12:45 -0500

    Deploy: Message feature

--- server/index.js
+++ server/index.js
@@ -19,54 +19,54 @@ const transport = {
   }
 }
 
-// const transporter = nodemailer.createTransport(transport);
+const transporter = nodemailer.createTransport(transport);
 
-// transporter.verify((error, success) => {
-//   if (error) {
-//     console.warn(error);
-//   } else {
-//     console.info('Server set to take messages');
-//   }
-// });
+transporter.verify((error, success) => {
+  if (error) {
+    console.warn(error);
+  } else {
+    console.info('Server set to take messages');
+  }
+});
 
-// router.post('/send', (req, res, next) => {
-//   const name = req.body.name
-//   const email = req.body.email
-//   const message = req.body.message
-//   const content = `name: ${name} \n email: ${email} \n message: ${message}`
+router.post('/send', (req, res, next) => {
+  const name = req.body.name
+  const email = req.body.email
+  const message = req.body.message
+  const content = `name: ${name} \n email: ${email} \n message: ${message}`
 
-//   const mail = {
-//     from: name,
-//     to: creds.USER,
-//     subject: 'JessaDaggs.com ~ You have a new message!',
-//     text: content
-//   }
+  const mail = {
+    from: name,
+    to: process.env.USER,
+    subject: 'JessaDaggs.com ~ You have a new message!',
+    text: content
+  }
 
-//   transporter.sendMail(mail, (err, data) => {
-//     if (err) {
-//       res.json({
-//         status: 'fail'
-//       })
-//     } else {
-//       res.json({
-//        status: 'success'
-//       })
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        status: 'fail'
+      })
+    } else {
+      res.json({
+       status: 'success'
+      })
 
-//       transporter.sendMail({
-//         from: creds.USER,
-//         to: email,
-//         subject: "JessaDaggs.com ~ Your email was sent to Jessa!",
-//         text: `Hey ${name},\nVery cool of you to reach out! This is an auto-reply...\nI have received your email and you will get a response from me soon.\n\nSending Good Vibes You Way,\nJessa Daggs\ndaggs.jessa@gmail.com\n504.405.0614`
-//       }, function(error, info){
-//         if(error) {
-//             console.warn(error);
-//         } else{
-//             console.info('Message sent: ' + info.response);
-//         }
-//       });
-//     }
-//   })
-// })
+      transporter.sendMail({
+        from: process.env.USER,
+        to: email,
+        subject: "JessaDaggs.com ~ Your email was sent to Jessa!",
+        text: `Hey ${name},\nVery cool of you to reach out! This is an auto-reply...\nI have received your email and you will get a response from me soon.\n\nSending Good Vibes You Way,\nJessa Daggs\ndaggs.jessa@gmail.com\n504.405.0614`
+      }, function(error, info){
+        if(error) {
+            console.warn(error);
+        } else{
+            console.info('Message sent: ' + info.response);
+        }
+      });
+    }
+  })
+})
 
 app.get('/', (req, res) => {
  res.sendFile(HTML_FILE);
